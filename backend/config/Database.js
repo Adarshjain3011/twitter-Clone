ac89db249ba38f25d0e7b77e468d57ad20165563
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-03-11 11:46:00 +0530

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -4,7 +4,14 @@ require("dotenv").config();
 
 const dbConnect = ()=>{
 
-    mongoose.connect("mongodb://127.0.0.1:27017/twitter",{}).then((data)=>{
+    // mongoose.connect("mongodb://127.0.0.1:27017/twitter",{}).then((data)=>{
+
+    console.log(
+        
+        process.env.DATABASE_URL
+    )
+
+    mongoose.connect(`mongodb+srv://adarshjain3011:97547@cluster0.q3yqnrh.mongodb.net/twitter`,{}).then((data)=>{
 
         console.log("database connected successfully ");
 
